40715bcbd48cda9ff80e0d3336b7d0c6c9462887
21120297 <21120297@student.hcmus.edu.vn>
2024-01-05 19:43:21 +0700

    staff setting

--- src/pages/User/UserHome/UserHome.jsx
+++ src/pages/User/UserHome/UserHome.jsx
@@ -24,11 +24,13 @@ const UserHome = () => {
   const [mainMenu, setMainMenu] = React.useState([]);
   const [invenMenu, setInvenMenu] = React.useState([]);
   const [originalMenu, setOriginalMenu] = React.useState([]);
-  const [currentCategory, setCurrentCategory] = React.useState('Các món ăn');
+  const [currentCategory, setCurrentCategory] = React.useState('Món chính');
   const [page, setPage] = React.useState(1);
   const [selectedCards, setSelectedCards] = React.useState([]);
-  const [time, setTime] = React.useState('06:00');
+  const [received, setReceived] = React.useState(0);
   const [total, setTotal] = React.useState(0);
+  const [change, setChange] = React.useState(0);
+  const [confirmPopup, setConfirmPopup] = React.useState(false);
   const [error, setError] = React.useState(false); 
   const [success, setSuccess] = React.useState(false); 
   const [message, setMessage] = React.useState('');
@@ -49,15 +51,17 @@ const UserHome = () => {
     });
     
     setTotal(sum);
+    setChange(received - sum);
   };
 
-  const handleTimeChange = (value) => {
-    setTime(value);
-  };
+  const handleReceivedChange = (value) => {
+    setReceived(value);
+    setChange(value - total);
+  }
 
   const handleQuantityChange = (cardId, quantity) => {
     const updatedSelectedCards = selectedCards.map((card) => {
-      if (card._id === cardId) {
+      if (card._id === cardId) { 
         if (quantity > card.item_quantity){
           quantity = card.item_quantity;
         }
@@ -71,7 +75,7 @@ const UserHome = () => {
   };
 
   const handleBreadcrumbClick = (category) => {
-    if (category == 'Các món ăn'){
+    if (category == 'Món chính'){
       setMenu(mainMenu);
       setOriginalMenu(mainMenu);
     }
@@ -85,49 +89,72 @@ const UserHome = () => {
 
   const handleClose = () => {
     setError(false);
-  };
+    setSelectedCards([]);
+  }
 
   const handleSuccess = () => {
+    const fetchMenuData = async () => {
+      try {
+        setLoading(true);
+        const inven = await itemsApi.getItemsByType({token, clientId}, 'inven');
+        setInvenMenu(inven.data);
+        const main = await itemsApi.getItemsByType({token, clientId}, 'main');
+        setMainMenu(main.data);
+        setMenu(main.data);
+        setOriginalMenu(main.data);
+        setLoading(false);
+      } catch (error) {
+      }
+    };
+
+    fetchMenuData();
     setSuccess(false);
     setSelectedCards([]);
-  };
+    setTotal(0);
+    setChange(0);
+    setReceived(0);
+  }
 
   const handleConfirm = () => {
     const createNewOrder = async () => {
+      setLoading(true);
+      const currentTime = new Date().toLocaleTimeString('en-US', { hour12: false });
       const transformedData = selectedCards.map(item => ({
-        item_name: item.item_name,
-        item_id: item._id,
-        item_quantity: item.quantity,
-        item_note: item.note || ''
-    }));
-      const res = await orderApi.createOrder({token, clientId}, transformedData, time);
+          item_name: item.item_name,
+          item_id: item._id,
+          item_quantity: item.quantity,
+          item_note: item.note || ''
+      }));
+      const res = await orderApi.createOrder({token, clientId}, transformedData, currentTime);
+      const confirm = await orderApi.confirmPayment({token, clientId}, res.data._id);
+      setLoading(false);
     };
 
-    if (time){
-      if (time <= '18:00' && time >= '06:00') {
-        if (selectedCards.length != 0){
-          createNewOrder();
-          setSuccess(true);
-        }
-        else {
-          setMessage('Đơn hàng không được bỏ trống');
-          setError(true);
-        }
+    if (change >= 0) {
+      if (selectedCards.length != 0){
+        createNewOrder();
+        setSuccess(true);
       }
-      else{
-        setMessage('Thời gian chọn không hợp lệ (6:00 AM - 6:00 PM).');
+      else {
+        setMessage('Đơn hàng không được bỏ trống');
         setError(true);
       }
-    } else{
-      setMessage('Thời gian chọn không hợp lệ (6:00 AM - 6:00 PM).');
+    }
+    else{
+      setMessage('Số tiền nhận vào không hợp lệ');
       setError(true);
     }
   };
 
+  const handleConfirmPopup = () => {
+    setConfirmPopup(false);
+    console.log("confirm");
+  }
+
   const handleCancel = () => {
     console.log("cancel");
   };
-  
+
   React.useEffect(() => {
     const fetchUser = async () => {
       try {
@@ -151,7 +178,6 @@ const UserHome = () => {
         setOriginalMenu(main.data);
         setLoading(false);
       } catch (error) {
-        
       }
     };
 
@@ -171,8 +197,7 @@ const UserHome = () => {
         }
     }
   }
-  
-  const titles = ['Các món ăn', 'Các món khác'];
+  const titles = ['Món chính', 'Các món khác'];
 
   const pageCount = Math.ceil(menu.length / itemsPerPage);
 
@@ -208,18 +233,16 @@ const UserHome = () => {
   };
 
   return (
-    <div className="relative grid grid-cols-3">
-      <div className="col-span-2">
-        <div>
-          <Header heading={user.name} />
-        </div>
-        <div className='col-span-1 p-3'>
+    <div className="relative grid grid-cols-[1fr_1fr_400px]">
+      <Header className='col-span-1' heading={user.name}></Header>
+      <div className='col-span-1 p-3'>
             <Searchbar
               handleSearch={handleSearchBar}  
               placeholder='Tìm tên...'
             />
-        </div>
-
+      </div>
+      <div></div>
+      <div className="col-span-2">
         <div className="ms-3">
           <Breadcrumbs aria-label="breadcrumb" className="p-2 me-5">
             {titles.map((title, index) => (
@@ -238,35 +261,33 @@ const UserHome = () => {
         </div>
 
         <div className="mt-5 p-2">
-          <Card sx={{ backgroundColor: 'background.tertiary' }}>
-            <CardHeader title="Chọn món" sx={{ color: 'white', fontSize: 20 }}>
-              Chọn món
-            </CardHeader>
+          <Card sx={{ backgroundColor: 'background.tertiary', marginLeft: '10px', boxShadow: 'none'}}>
             <Grid container spacing={3}>
-            {(menu.slice((page - 1) * itemsPerPage, page * itemsPerPage) || []).map((item) => (
+              {(menu.slice((page - 1) * itemsPerPage, page * itemsPerPage) || []).map((item) => (
                 <Grid item key={item._id} xs={12} sm={6} md={4}>
                   <Card sx={{ 
                     border: 'rounded',
                     color: 'white', 
                     bgcolor: '#1F1D2B',
-                    borderRadius: '12px', 
+                    borderRadius: '12px',
                     '&:hover': {
                       cursor: 'pointer',
                       bgcolor: 'background.tertiary',
                   },}}
                     onClick={() => handleCardClick(item)}
                   >
-                     <CardMedia 
+                    <CardMedia 
                       component="img" 
                       image={item.item_image} 
                       alt={item._id}
                       sx={{
                         maxWidth: '100%',
-                        maxHeight: '100px',
+                        height: '200px',
+                        objectFit: 'fill'
                       }}
                     />
                     <CardContent sx={{ textAlign: 'center' }}>
-                      <Typography>{item.item_name}</Typography>
+                      <Typography className='text-primary'>{item.item_name}</Typography>
                       <Typography>{item.item_price}đ</Typography>
                       <Typography>Số lượng còn lại: {item.item_quantity}</Typography>
                     </CardContent>
@@ -282,6 +303,7 @@ const UserHome = () => {
               onChange={handleChangePage}
               color="primary"
               size="large"
+              shape='rounded'
               sx={{ mx: 'auto'}}
             />
           </Stack>
@@ -289,15 +311,15 @@ const UserHome = () => {
       </div>
 
       <Card className="col-span-1 fixed right-6 w-1/4 h-screen p-4 rounded-lg" sx={{ color: 'white', minWidth: '400', backgroundColor: 'background.secondary' }}>
-        <div style={{ display: 'grid', gridTemplateColumns: '45% 30% 20%', gridColumnGap: '10px', gridRowGap: '8px', marginBottom: '16px', fontWeight: 'bold' }}>
+        <div style={{ display: 'grid', gridTemplateColumns: '45% 30% 20%', gridColumnGap: '10px', griditemGap: '8px', marginBottom: '16px', fontWeight: 'bold' }}>
             <Typography>Sản phẩm</Typography>
             <Typography>Số lượng</Typography>
             <Typography>Giá</Typography>
           </div>
-        <div style={{ maxHeight: 'calc(100vh - 260px)', overflowY: 'auto' }}>
+        <div style={{ maxHeight: 'calc(100vh - 300px)', overflowY: 'auto' }}>
           {selectedCards.map((selectedCard) => (
             <div key={selectedCard._id}>
-              {/* First Row */}
+              {/* First item */}
               <div style={{ display: 'grid', gridTemplateColumns: '50% 20% 20%', gridColumnGap: '10px', marginBottom: '2px', alignItems: 'center' }}>
                 <Card sx={{ 
                   backgroundColor: 'background.secondary',
@@ -319,10 +341,10 @@ const UserHome = () => {
                   </CardContent>
                 </Card>
                 <Input
-                  className='w-10'
+                  className='w-full -ms-3'
                   type="number"
                   value={selectedCard.quantity || 1}
-                  inputProps={{ min: 1, max: selectedCard.item_quantity }}
+                  inputProps={{ min: 1 }}
                   onChange={(e) => handleQuantityChange(selectedCard._id, e.target.value)}
                   sx={{
                     borderRadius: '4px',
@@ -331,9 +353,9 @@ const UserHome = () => {
                     color: 'white'
                   }}
                 />
-                <Typography>{parseInt(selectedCard.quantity) * parseInt(selectedCard.item_price)}</Typography>
+                <Typography>{`${parseInt(selectedCard.quantity) * parseInt(selectedCard.item_price)} đ`}</Typography>
               </div>
-              {/* Second Row */}
+              {/* Second item */}
               <div style={{ display: 'grid', gridTemplateColumns: '70% 20%', gridColumnGap: '16px',  marginBottom: '4px', alignItems: 'center' }}>
               <Input
                   type="text"
@@ -365,7 +387,7 @@ const UserHome = () => {
           ))}
         </div>
 
-        <div className="absolute left-4 bottom-6">
+        <div className="absolute left-0 right-0 w-full bottom-6">
         <TableContainer>
         <Table sx={{ minWidth: 300, bgcolor: 'background.secondary' }} size="small">
           <TableBody>
@@ -374,32 +396,61 @@ const UserHome = () => {
               <TableCell sx={{color: 'white'}}>{total}đ</TableCell>
             </TableRow>
             <TableRow>
-              <TableCell sx={{color: 'white',  minWidth: 150}}>Thời gian đến</TableCell>
+              <TableCell sx={{color: 'white', minWidth: 150}}>Đã nhận</TableCell>
               <TableCell>
                 <Input
-                  type="time"
-                  value={time}
-                  onChange={(e) => handleTimeChange(e.target.value)}
+                  type="number"
+                  value={received}
+                  onChange={(e) => handleReceivedChange(e.target.value)}
                   style={{color: 'white'}}
                 />
               </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell sx={{color: 'white', minWidth: 150}}>Tiền thừa</TableCell>
+              <TableCell sx={{color: 'white'}}>{change}đ</TableCell>
             </TableRow>
           </TableBody>
         </Table>
       </TableContainer>
         <CustomButton
-          title={'Đặt món'}
+          title={'Xác nhận thanh toán'}
           className="p-2 mt-2 w-full"
           onAction={handleConfirm}
+          variant='tertiary'
         />
         <CustomButton
           title={'Hủy'}
           className='p-2 mt-2 w-full'
           onAction={handleCancel}
+          variant='secondary'
         />
         </div>
       </Card>
-
+      <PopUp
+        title="Xác nhận"
+        isOpen={confirmPopup}
+        handleCloseBtnClick={() => {setConfirmPopup(false);}}
+      >
+        {
+          <div className='flex flex-col'>
+            <h2 className='text-white pb-5'>Bạn có muốn xác nhận thanh toán?</h2>
+            <div className='flex justify-between gap-2'>
+              <CustomButton
+                title='Hủy'
+                variant='secondary'
+                onAction={()=>{setConfirmPopup(false);}}
+                className="py-1 px-8"
+              />
+              <CustomButton
+                title='Xác nhận'
+                onAction={handleConfirmPopup}
+                className="py-1 px-4"
+              />
+            </div>
+          </div>
+        }
+      </PopUp>
       <Dialog open={error} onClose={handleClose}>
         <DialogTitle sx={{ backgroundColor: 'background.tertiary' }}>Lỗi</DialogTitle>
         <DialogContent sx={{ backgroundColor: 'background.tertiary' }}>
@@ -418,7 +469,7 @@ const UserHome = () => {
         <DialogTitle sx={{ backgroundColor: 'background.tertiary' }}>Thành công</DialogTitle>
         <DialogContent sx={{ backgroundColor: 'background.tertiary' }}>
           <DialogContentText sx={{color: 'white'}}>
-            Bạn đã đặt đơn hàng thành công
+            Thanh toán đơn hàng thành công
           </DialogContentText>
         </DialogContent>
         <DialogActions sx={{ backgroundColor: 'background.tertiary' }}>
@@ -427,6 +478,10 @@ const UserHome = () => {
           </Button>
         </DialogActions>
       </Dialog>
+
+      
+
+
       {isLoading && <Loading/>}
     </div>
   );
