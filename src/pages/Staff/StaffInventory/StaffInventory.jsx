a0bc96d140b0bf7c13414e8a590ebdee1a180102
21120297 <21120297@student.hcmus.edu.vn>
2024-01-06 00:18:47 +0700

    fix expired product

--- src/pages/Staff/StaffInventory/StaffInventory.jsx
+++ src/pages/Staff/StaffInventory/StaffInventory.jsx
@@ -1,120 +1,139 @@
-import React from 'react';
-import InventoryApi from '../../../api/inventoryApi';
-import { useStaffInventoryContext } from '../../../context/Staff/StaffInventoryContext';
-import Header from "../../../components/Header/Header"
+import React from "react";
+import InventoryApi from "../../../api/inventoryApi";
+import { useStaffInventoryContext } from "../../../context/Staff/StaffInventoryContext";
+import Header from "../../../components/Header/Header";
 import ExpiredProductTable from "./ExpiredProductTable/ExpiredProductTable";
 import InventoryTable from "./InventoryTable/InventoryTable";
-import CustomButton from '../../../components/CustomButton/CustomButton';
-import PopUp from '../../../components/Popup/Popup';
-import GRNForm from './GoodsReceivedNoteForm/GRNForm';
-import GDNForm from './GoodsDeliveryNoteForm/GDNForm';
+import CustomButton from "../../../components/CustomButton/CustomButton";
+import PopUp from "../../../components/Popup/Popup";
+import GRNForm from "./GoodsReceivedNoteForm/GRNForm";
+import GDNForm from "./GoodsDeliveryNoteForm/GDNForm";
 
 const StaffInventory = () => {
   //Inventory context
   const {
-    setInventoryTableRows, setInventoryTableOriginalRows, setExpiredTableRows, setExpiredTableOriginalRows 
+    setInventoryTableRows,
+    setInventoryTableOriginalRows,
+    setExpiredTableRows,
+    setExpiredTableOriginalRows,
   } = useStaffInventoryContext();
 
   const [isImportPopUpOpen, setImportPopUpOpen] = React.useState(false);
   const [isExportPopUpOpen, setExportPopUpOpen] = React.useState(false);
- 
+
   const headCells = [
     {
-      id: '_id',
+      id: "_id",
       numeric: false,
       disablePadding: true,
-      label: 'Mã sản phẩm',
+      label: "Mã sản phẩm",
     },
     {
-      id: 'inventoryItem_img',
+      id: "inventoryItem_img",
       numeric: false,
       disablePadding: true,
-      label: 'Ảnh',
+      label: "Ảnh",
     },
     {
-      id: 'inventoryItem_name',
+      id: "inventoryItem_name",
       numeric: false,
       disablePadding: true,
-      label: 'Tên sản phẩm',
+      label: "Tên sản phẩm",
     },
     {
-      id: 'inventoryItem_quantity',
+      id: "inventoryItem_quantity",
       numeric: true,
       disablePadding: true,
-      label: 'Số lượng',
+      label: "Số lượng",
     },
     {
-      id: 'cost',
+      id: "cost",
       numeric: true,
       disablePadding: true,
-      label: 'Giá nhập',
+      label: "Giá nhập",
     },
     {
-      id: 'inventoryItem_exp',
+      id: "inventoryItem_exp",
       numeric: false,
       disablePadding: true,
-      label: 'Hạn sử dụng',
+      label: "Hạn sử dụng",
     },
   ];
 
   const handleCreateGRN = (list) => {
-    const createGoodReceiveNote = async() => {
+    const createGoodReceiveNote = async () => {
       try {
-        const token = localStorage.getItem('token');
-        const clientId = localStorage.getItem('clientId');
+        const token = localStorage.getItem("token");
+        const clientId = localStorage.getItem("clientId");
         const body = {
-          "userId": clientId,
-          "item_list": list
-        }
-        const res = await InventoryApi.createGoodReceiveNote({token, clientId}, body);
+          userId: clientId,
+          item_list: list,
+        };
+        const res = await InventoryApi.createGoodReceiveNote(
+          { token, clientId },
+          body
+        );
 
         //Update Inventory Table
-        const invenRes = await InventoryApi.getAllInventoryItems({token, clientId});
+        const invenRes = await InventoryApi.getAllInventoryItems({
+          token,
+          clientId,
+        });
         const data = invenRes.data;
         setInventoryTableRows(data);
         setInventoryTableOriginalRows(data);
 
         //Update expired product table
-        const expiredRes = await InventoryApi.getAllInventoryItems({token, clientId});
+        const expiredRes = await InventoryApi.getAllExpiredProduct({
+          token,
+          clientId,
+        });
         setExpiredTableRows(expiredRes.data);
         setExpiredTableOriginalRows(expiredRes.data);
-      } 
-      catch (error) {
+      } catch (error) {
         //
       }
-    }
+    };
 
     createGoodReceiveNote();
-  }
+  };
 
   const handleCreateGDN = (list) => {
-    const createGoodDeliveryNote = async() => {
+    const createGoodDeliveryNote = async () => {
       try {
-        const token = localStorage.getItem('token');
-        const clientId = localStorage.getItem('clientId');
+        const token = localStorage.getItem("token");
+        const clientId = localStorage.getItem("clientId");
         const body = {
-          "userId": clientId,
-          "item_list": list
-        }
-        const res = await InventoryApi.createGoodDeliveryNote({token, clientId}, body);
+          userId: clientId,
+          item_list: list,
+        };
+        const res = await InventoryApi.createGoodDeliveryNote(
+          { token, clientId },
+          body
+        );
 
         //Update Inventory Table
-        const invenRes = await InventoryApi.getAllInventoryItems({token, clientId});
+        const invenRes = await InventoryApi.getAllInventoryItems({
+          token,
+          clientId,
+        });
         const data = invenRes.data;
         setInventoryTableRows(data);
         setInventoryTableOriginalRows(data);
 
         //Update expired product table
-        const expiredRes = await InventoryApi.getAllExpiredProduct({token, clientId});
+        const expiredRes = await InventoryApi.getAllExpiredProduct({
+          token,
+          clientId,
+        });
         setExpiredTableRows(expiredRes.data);
         setExpiredTableOriginalRows(expiredRes.data);
-      } 
-      catch (error) {
+      } catch (error) {
         //
       }
-    }
+    };
     createGoodDeliveryNote();
-  }
+  };
 
   return (
     <>
@@ -122,30 +141,28 @@ const StaffInventory = () => {
         <Header heading="Quản lý kho"></Header>
       </div>
       <div className="ms-3">
-        <CustomButton 
-          title={'Tạo phiếu nhập kho'}
+        <CustomButton
+          title={"Tạo phiếu nhập kho"}
           className="p-2 me-5"
-          onAction={() => {setImportPopUpOpen(true)}}
+          onAction={() => {
+            setImportPopUpOpen(true);
+          }}
         />
-        <CustomButton 
-          title={'Tạo phiếu xuất kho'}
+        <CustomButton
+          title={"Tạo phiếu xuất kho"}
           className="p-2"
-          onAction={() => {setExportPopUpOpen(true)}}
-        />
-      </div>
-      
-      <div className="mt-5 p-2">
-        <InventoryTable 
-          headCells={headCells} 
-          title={'Kho'} 
+          onAction={() => {
+            setExportPopUpOpen(true);
+          }}
         />
       </div>
 
       <div className="mt-5 p-2">
-        <ExpiredProductTable 
-          headCells={headCells} 
-          title={'Sản phẩm hết hạn'} 
-        />
+        <InventoryTable headCells={headCells} title={"Kho"} />
+      </div>
+
+      <div className="mt-5 p-2">
+        <ExpiredProductTable headCells={headCells} title={"Sản phẩm hết hạn"} />
       </div>
 
       <PopUp
@@ -153,7 +170,7 @@ const StaffInventory = () => {
         isOpen={isImportPopUpOpen}
         handleCloseBtnClick={() => setImportPopUpOpen(false)}
       >
-        <GRNForm 
+        <GRNForm
           closePopUp={() => setImportPopUpOpen(false)}
           onSubmit={handleCreateGRN}
         />
@@ -164,7 +181,7 @@ const StaffInventory = () => {
         isOpen={isExportPopUpOpen}
         handleCloseBtnClick={() => setExportPopUpOpen(false)}
       >
-        <GDNForm 
+        <GDNForm
           closePopUp={() => setExportPopUpOpen(false)}
           onSubmit={handleCreateGDN}
         />
